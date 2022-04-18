c23d24651bbdc7e726a57fa39f67d98c74874de1
LeoG <guoyou0105@163.com>
2022-04-19 00:08:57 +0800

    master git push test

--- src/main/java/com/guoyou/git/GitTest.java
+++ src/main/java/com/guoyou/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("master conflict test!");
         System.out.println("hot-fix conflict test1");
+        System.out.println("push test!");
     }
 }
